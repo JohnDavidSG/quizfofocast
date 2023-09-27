3fc5538022e096e30ed6d3955d46e5fdf8a89669
JohnDavidSG <141514058+JohnDavidSG@users.noreply.github.com>
2023-09-27 11:33:58 -0300

    Subindo arquivos

---
+++ questions.js
@@ -0,0 +1,43 @@
+export default [
+    {
+      question: "O Sistema nervoso é importante para o corpo humano?",
+      answers: [
+        { option: "Não!", correct: false },
+        { option: "Outra reposta:", correct: false },
+        { option: "Sim!", correct: true },
+      ],
+    },
+    {
+      question: "O uso inadequaldo de anabolizante é prejudicial?",
+      answers: [
+        { option: "Não!", correct: false },
+        { option: "Sim!", correct: true },
+        { option: "Outra resposta:", correct: false },
+      ],
+    },
+    {
+      question: "Miguel Falabella usou anabolizantes em que ano?",
+      answers: [
+        { option: "1998", correct: true },
+        { option: "2010", correct: false },
+        { option: "1995", correct: false },
+      ],
+    },
+    {
+      question: "Qual destas teorias da origem da vida é mais aceita?",
+      answers: [
+        { option: "Abiogenese", correct: false },
+        { option: "Biogenese", correct: true },
+        { option: "Teoria do Criacionismo", correct: false },
+      ],
+    },
+    {
+      question: "Oque pode prejudicar o sistema nervoso?",
+      answers: [
+        { option: "Drogas, boa alimentação, passar no psicologo, etc.", correct: false },
+        { option: "Drogas, Acool em excesso, sedentarismo, alimentação ruim, etc.", correct: true },
+        { option: "Alcool em excesso, vivacidade, alimentação ruim, Drogas, etc. ", correct: false },
+      ],
+    },
+  ];
+  
